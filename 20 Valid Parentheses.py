292806a2eb026d7c26ad45bb4820ee1eeebbd5a3
sparkerver002 <116824606+sparkerver002@users.noreply.github.com>
2022-10-27 20:39:49 +0530

    Create 20 Valid Parentheses.py

---
+++ 20 Valid Parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_list = ["[","{","("]
+        close_list = ["]","}",")"]
+        stack = []
+        for i in s:
+            if i in open_list:
+                stack.append(i)
+            elif i in close_list:
+                pos = close_list.index(i)
+                if ((len(stack) > 0) and
+                    (open_list[pos] == stack[len(stack)-1])):
+                    stack.pop()
+                else:
+                    return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
